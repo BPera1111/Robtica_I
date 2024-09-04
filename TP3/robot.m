264a666350e0df95d5284fb7dbbb037ed9703021
BPera1111 <bruno.peralta@ms.uncu.edu.ar>
2024-09-04 19:43:27 -0300

    robot bien definido

---
+++ TP3/robot.m
@@ -0,0 +1,39 @@
+function robot; clc; clear ; close all; %#ok<*CLEAR0ARGS,*NOPTS>
+    
+    dh_kuka_16=[0.000 0.675 0.260   pi/2  0;
+                0.000 0.000 0.680   0     0;
+                0.000 0.000 0.035   pi/2  0;
+                0.000 0.670 0.000  -pi/2  0;
+                0.000 0.000 0.000   pi/2  0;
+                0.000 0.158 0.000   0     0];
+
+    q_kuka_16=[0,0,0,0,0,0];
+
+    qlim_kuka_16=[-92.5, 92.5; 
+                  -155, 35; 
+                  -130, 154; 
+                  -175, 175; 
+                  -65, 65; 
+                  -175, 175]*pi/180;
+    offet = [0,pi/2,0,0,0,0];
+
+    base = transl(-1,1,0) * trotz(-45);
+    path = fullfile(pwd,'STL','KR16_arc_HW')
+    % figure('name', 'Kuka 16');
+    R=create_robot(dh_kuka_16, 'Kuka 16', q_kuka_16, qlim_kuka_16, offet, base,path);
+    save('kuka_16.mat', 'R',"q_kuka_16","qlim_kuka_16","offet","base","path");
+
+    
+    
+end
+
+function R =create_robot(dh, name, q, qlim, offset,base,path)
+    R = SerialLink(dh, 'name', name);
+    R.base = base;
+    % R.tool = transl(.2, .2, 0);
+    R.offset = offset;
+    R.qlim = qlim;
+    % R.plot(q, 'scale', 0.8, 'trail', {'r', 'LineWidth', 2});
+    % R.teach();
+    % R.plot3d(q, 'path',path, 'nowrist', 'noarrow', 'view', [-30 30], 'delay', 0.01);
+end
---
+++ TP3/test.m
@@ -0,0 +1,15 @@
+% Cargar el objeto R desde el archivo .mat
+
+function test; clc; clear ; close all; %#ok<*CLEAR0ARGS,*NOPTS>
+    load('kuka_16.mat', 'R','q_kuka_16','path');
+
+
+    % figure('name', 'Kuka 16 con stl');
+    % R.plot3d(q_kuka_16, 'path',path, 'nowrist','view',[30,30], 'scale', 0.1);
+
+
+    figure('name', 'Kuka 16 con teach');
+    R.plot(q_kuka_16, 'scale', 0.4, 'trail', {'r', 'LineWidth', 2});
+    R.teach();
+end
+
