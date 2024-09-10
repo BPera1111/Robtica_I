7b2d4a3281e5088c02c73ab543bf7fd3fb14f1f7
nacho <ignaciobabolene13@gmail.com>
2024-09-09 23:24:27 -0300

    TP4 ejercicio 2 bien chachi

---
+++ TP4/TP4_2.m
@@ -0,0 +1,22 @@
+% Trabajo Práctico N4 Ejercicio 2
+close all; clc; clear;
+DH = [
+ 0.000 0.195  0.300 0.000 0;
+ 0.000 0.000  0.250 0.000 0;
+ 0.000 0.000  0.000 pi    1;
+ 0.000 0.000  0.000 0.000 0];
+DH_nueva = [
+ 0.000, 0.262,  0.200, 0.000, 0;
+ 0.000, 0.000,  0.250, 0.000, 0;
+ 0.000, 0.000,  0.000, pi,    0;
+ 0.000, 0.000,  0.000, 0.000, 1];
+ R = SerialLink(DH);
+ q = [0,0,0,0];
+ T = double(R.fkine(q));
+ disp('Matriz de transformación homogénea con parámetros dados:')
+ disp(T)
+ R_n = SerialLink(DH_nueva);
+ q_n = [0,0,0,0];
+ T_nueva = double(R_n.fkine(q_n));
+ disp('Matriz de transformación homogénea con parámetros obtenidos:')
+ disp(T_nueva)
