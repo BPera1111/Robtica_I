b74f19b7488bd1ebd7bf71a077cf372dc125ab77
BPera1111 <bruno.peralta@ms.uncu.edu.ar>
2024-11-09 02:37:16 -0300

    ready tf?

--- TF/TF.m
+++ TF/TF.m
@@ -26,96 +26,51 @@ qi_d = [0 0 0 0 0 0]*pi/180;
 
 disp('trayectoria izq')
 [tra_i vms_i] = ejecutar_trayectoria_soldadura_alineada_izq(Ri,1,tx_i,ty_i,tz_i,data,qi_i);
-% disp(tra_i*180/pi)
+
 disp('trayectoria der')
 [tra_d vms_d] = ejecutar_trayectoria_soldadura_alineada_izq(Rd,1,tx_d,ty_d,tz_d,data,qi_d);
-% disp(tra_d*180/pi)
 
-% primer matriz
-% disp(fkine(Ri,tra_i(1,:)))
+
+
 
 %% interpolación articular izq y der
-q_td = []; q_vd = []; q_ad = []; points = 2;
-for i = 1:size(tra_d, 1)-1
-    [q_d, qd_d, qdd_d] = jtraj(tra_d(i,:), tra_d(i+1, :), points);
-    q_td = [q_td; q_d];
-    q_vd = [q_vd; qd_d];
-    q_ad = [q_ad; qdd_d];
-end
-q_ti = []; q_vi = []; q_ai = [];
-for i = 1:size(tra_i, 1)-1
-    [q_i, qd_i, qdd_i] = jtraj(tra_i(i,:), tra_i(i+1, :), points);
-    q_ti = [q_ti; q_i];
-    q_vi = [q_vi; qd_i];
-    q_ai = [q_ai; qdd_i];
-end
-
-%% interpolación cartesiana izq y der con mstraj
-% Parámetros de la trayectoria cartesiana
-% vel = 0.1; % Velocidad máxima (puede ser un vector si se requiere diferente velocidad por dimensión)
-% dt = 1; % Intervalo de tiempo entre puntos
-% accel = 0.05; % Aceleración máxima
-% % q0 = [90.0000   43.1370  -65.4540   90.0000 -108.3170  -90.0000]*pi/180; % Posición inicial
-% % q0 = TP5B_EjercicioTF(homogenousTransform(vms_i(1, 1), vms_i(1, 2), vms_i(1, 3), vms_i(1, 4), vms_i(1, 5), vms_i(1, 6)), Ri,  [0 0 0 0 0 0]*pi/180, true)
-% q0 = vms_i(1, :);
-
-% % Generar la trayectoria
-% traj_xyzrpy = mstraj(vms_i, vel, [], q0, dt, accel);
-
-% % disp('vms_i')
-% % disp(vms_i(1,:))
-% % disp('traj_xyzrpy')
-% % disp(traj_xyzrpy(1,:))
-% % disp( homogenousTransform(traj_xyzrpy(1, 1), traj_xyzrpy(1, 2), traj_xyzrpy(1, 3), traj_xyzrpy(1, 4), traj_xyzrpy(1, 5), traj_xyzrpy(1, 6)) )
-
-% traj_articular = zeros(size(traj_xyzrpy, 1), Ri.n);
-% for i = 1:size(traj_xyzrpy, 1)
-%     T = homogenousTransform(traj_xyzrpy(i, 1), traj_xyzrpy(i, 2), traj_xyzrpy(i, 3), traj_xyzrpy(i, 4), traj_xyzrpy(i, 5), traj_xyzrpy(i, 6));
-%     % q = Ri.ikine(T, 'mask', [1 1 1 0 0 0]);
-%     if i == 1
-%         q = TP5B_EjercicioTF(T, Ri, [0 0 0 -90 0 0]*pi/180, true);
-%     end
-%     if i > 1
-%         q = TP5B_EjercicioTF(T, Ri, traj_articular(i-1, :), true);
-%     end
-%     traj_articular(i, :) = q;
+% q_td = []; q_vd = []; q_ad = []; points = 2;
+% for i = 1:size(tra_d, 1)-1
+%     [q_d, qd_d, qdd_d] = jtraj(tra_d(i,:), tra_d(i+1, :), points);
+%     q_td = [q_td; q_d];
+%     q_vd = [q_vd; qd_d];
+%     q_ad = [q_ad; qdd_d];
 % end
-% traj_articular
-% % q_ti
-% T_pre = homogenousTransform(traj_xyzrpy(1, 1), traj_xyzrpy(1, 2), traj_xyzrpy(1, 3), traj_xyzrpy(1, 4), traj_xyzrpy(1, 5), traj_xyzrpy(1, 6))
-% T_post = Ri.fkine(traj_articular(1, :))
-
-traj_articular_i = inter_mstraj(Ri, vms_i);
-traj_articular_d = inter_mstraj(Rd, vms_d);
-
-plotada(Ri,traj_articular_i,q_vi,q_ai,trayectoria_x,trayectoria_y,trayectoria_z)
-plotada(Rd,traj_articular_d,q_vd,q_ad,trayectoria_x,trayectoria_y,trayectoria_z)
-
-% figure;
-% plot3(traj_xyzrpy(:, 1), traj_xyzrpy(:, 2), traj_xyzrpy(:, 3), 'r');
-% xlabel('X'); ylabel('Y'); zlabel('Z');
-% title('Trayectoria en el espacio cartesiano');
-% grid on;
+% q_ti = []; q_vi = []; q_ai = [];
+% for i = 1:size(tra_i, 1)-1
+%     [q_i, qd_i, qdd_i] = jtraj(tra_i(i,:), tra_i(i+1, :), points);
+%     q_ti = [q_ti; q_i];
+%     q_vi = [q_vi; qd_i];
+%     q_ai = [q_ai; qdd_i];
+% end
 
 
-function traj_articular = inter_mstraj(R, vms)
+disp('Interpolación articular izq')
+[t_a_i v_a_i a_a_i] = inter_mstraj(Ri, vms_i);
+disp('Interpolación articular der')
+[t_a_d v_a_d a_a_d] = inter_mstraj(Rd, vms_d);
+
+plotada(Ri,t_a_i,v_a_i,a_a_i,trayectoria_x,trayectoria_y,trayectoria_z ,' Robot izquierdo')
+plotada(Rd,t_a_d,v_a_d,a_a_d,trayectoria_x,trayectoria_y,trayectoria_z, ' Robot derecho')
+
+
+
+
+function [traj_articular qd_num qdd_num] = inter_mstraj(R, vms)
     %% interpolación cartesiana izq y der con mstraj
-    % Parámetros de la trayectoria cartesiana
+    %% Parámetros de la trayectoria cartesiana
     vel = 0.1; % Velocidad máxima (puede ser un vector si se requiere diferente velocidad por dimensión)
-    dt = 1; % Intervalo de tiempo entre puntos
+    dt = 0.01; % Intervalo de tiempo entre puntos
     accel = 0.05; % Aceleración máxima
-    % q0 = [90.0000   43.1370  -65.4540   90.0000 -108.3170  -90.0000]*pi/180; % Posición inicial
-    % q0 = TP5B_EjercicioTF(homogenousTransform(vms_i(1, 1), vms_i(1, 2), vms_i(1, 3), vms_i(1, 4), vms_i(1, 5), vms_i(1, 6)), Ri,  [0 0 0 0 0 0]*pi/180, true)
     q0 = vms(1, :);
 
     % Generar la trayectoria
     traj_xyzrpy = mstraj(vms, vel, [], q0, dt, accel);
-
-    % disp('vms_i')
-    % disp(vms_i(1,:))
-    % disp('traj_xyzrpy')
-    % disp(traj_xyzrpy(1,:))
-    % disp( homogenousTransform(traj_xyzrpy(1, 1), traj_xyzrpy(1, 2), traj_xyzrpy(1, 3), traj_xyzrpy(1, 4), traj_xyzrpy(1, 5), traj_xyzrpy(1, 6)) )
 
     traj_articular = zeros(size(traj_xyzrpy, 1), R.n);
     for i = 1:size(traj_xyzrpy, 1)
@@ -127,7 +82,25 @@ function traj_articular = inter_mstraj(R, vms)
         if i > 1
             q = TP5B_EjercicioTF(T, R, traj_articular(i-1, :), true);
         end
+        q(6) = 0; % fijamos q6
         traj_articular(i, :) = q;
+    end
+
+    %% calculo de la velocidad y aceleración
+    [row, col] = size(traj_articular);
+    qd_num = zeros(size(traj_articular));
+    qdd_num = zeros(size(traj_articular));
+    m = size(traj_articular, 1)
+    for c = 1:col
+        for r = 2:row-1
+        qd_num(r,c) = (traj_articular(r+1,c) - traj_articular(r,c))*m; 
+        end
+    end
+
+    for c = 1:col
+        for r = 2:row-1
+        qdd_num(r,c) = (qd_num(r+1,c) - qd_num(r,c))*m; 
+        end
     end
 
 end
@@ -318,38 +291,38 @@ function mostrar_simple_trayectoria(R,traj,tray_x,tray_y,tray_z)
     end
 end
 
-function plotada(R,q,v,a,cx,cy,cz)
+function plotada(R,q,v,a,cx,cy,cz,robot)
+
+    % figure();
+    % title(['Trayectoria circular en el plano YZ ' robot]);
+    % plot3(cx,cy,cz,'b','LineWidth',1.5);
+    % hold on;
+    % axis([-2 2 -2 2 0 2]);
+    % path = fullfile(pwd,'..','STL','KR16_2');
+    % R.plot3d(q, 'path',path,'notiles', 'nowrist','view',[90,0], 'scale', 0.1);
+    % hold off;
 
     figure();
-    title('Trayectoria circular en el plano YZ');
-    plot3(cx,cy,cz,'b','LineWidth',1.5);
-    hold on;
-    axis([-2 2 -2 2 0 2]);
-    path = fullfile(pwd,'..','STL','KR16_2');
-    R.plot3d(q, 'path',path,'notiles', 'nowrist','view',[90,0], 'scale', 0.1);
-    hold off;
-
-    figure();
-    title('Posición')
+    title(['Posición' robot])
     qplot(q)
     ylabel('Posición (rad)')
     xlabel('Tiempo (s)')
     grid on
 
 
-    % figure();
-    % title('Velocidad')
-    % qplot(v)
-    % ylabel('Velocidad (rad/s)')
-    % xlabel('Tiempo (s)')
-    % grid on
+    figure();
+    title(['Velocidad' robot])
+    qplot(v)
+    ylabel('Velocidad (rad/s)')
+    xlabel('Tiempo (s)')
+    grid on
 
-    % figure();
-    % title('Aceleración')
-    % qplot(a)
-    % ylabel('Aceleración (rad/s^2)')
-    % xlabel('Tiempo (s)')
-    % grid on
+    figure();
+    title(['Aceleración' robot])
+    qplot(a)
+    ylabel('Aceleración (rad/s^2)')
+    xlabel('Tiempo (s)')
+    grid on
 end
 
 function T = homogenousTransform(X, Y, Z, roll, pitch, yaw)
